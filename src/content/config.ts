24f9e036a8e17cf6830361a405257a3b25c85444
eduardowke <eduardokr@gmail.com>
2026-09-23 11:21:28 -0300

    Configura galeria dinâmica de artes  com Content Collections

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const artesCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    date: z.date().or(z.string().transform((val) => new Date(val))),
+    image: z.string(),
+    style: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  artes: artesCollection,
+};
